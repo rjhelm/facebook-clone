b8e70f49e2d62b672db5adef555adb5b56b8672b
Ryan Helm <rjhelm@github.io>
2021-05-26 19:51:46 -0600

    started header section

---
+++ src/Header.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar } from '@material-ui/core';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
+                <div className="header_input">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="header_middle">
+                <div className="header_option">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <SubscriptionsIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <StorefrontIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
+
+            <div className="header_right">
+                <div className="header_info">
+                    <Avatar />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAsVoByCwtaLoKWdjVUXKWhQl67QSojHag",
+    authDomain: "facebook-clone-ryanjdev.firebaseapp.com",
+    projectId: "facebook-clone-ryanjdev",
+    storageBucket: "facebook-clone-ryanjdev.appspot.com",
+    messagingSenderId: "814333921138",
+    appId: "1:814333921138:web:6c4e7115bd75f1e53bffeb",
+    measurementId: "G-92SEKN8FVM"
+};
